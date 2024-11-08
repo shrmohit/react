71924c615e96fb701b0c5ca10db01d2ae7df1aa8
Mohit Sharma <mohitsharma7258@gmail.com>
2024-11-09 03:06:30 +0530

    footer

---
+++ navbar/src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AiFillInstagram } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+
+
+function Footer() {
+  return (
+    <div>
+        <AiFillInstagram />
+        <AiOutlineGithub />
+    </div>
+  )
+}
+
+export default Footer
